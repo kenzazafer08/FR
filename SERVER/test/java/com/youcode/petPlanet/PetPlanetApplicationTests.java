07344808d6d5a8c2075c5d1839f69684d7f90226
kenza zafer <112939449+kenzazafer08@users.noreply.github.com>
2024-03-22 09:21:53 +0000

    Add files via upload

---
+++ SERVER/test/java/com/youcode/petPlanet/PetPlanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youcode.petPlanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPlanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
