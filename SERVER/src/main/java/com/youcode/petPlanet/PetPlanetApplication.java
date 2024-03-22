f086eb608bb8d90119052a9ab50aed19603f19aa
kenza zafer <112939449+kenzazafer08@users.noreply.github.com>
2024-03-22 09:16:56 +0000

    Add files via upload

---
+++ SERVER/src/main/java/com/youcode/petPlanet/PetPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.youcode.petPlanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetPlanetApplication.class, args);
+	}
+
+}
